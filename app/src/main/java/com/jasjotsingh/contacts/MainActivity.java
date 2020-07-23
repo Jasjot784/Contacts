553d9a83308009c07b9b37df3ecb4f1f65dcef07
Jasjot Singh <jasjot784@gmail.com>
2020-07-23 18:14:23 +0530

    Call us implemented

--- app/src/main/java/com/jasjotsingh/contacts/MainActivity.java
+++ app/src/main/java/com/jasjotsingh/contacts/MainActivity.java
@@ -2,7 +2,11 @@ package com.jasjotsingh.contacts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.buttonContact).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callUs();
+            }
+        });
+    }
+    public void callUs(){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        String phoneNumber = "+919463809630";
+        String formattedNumber = String.format("tel: %s", phoneNumber);
+        intent.setData(Uri.parse(formattedNumber));
+
+        if (intent.resolveActivity(getPackageManager())!=null)
+            startActivity(intent);
     }
 }
