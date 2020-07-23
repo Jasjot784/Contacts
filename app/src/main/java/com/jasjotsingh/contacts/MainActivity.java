e0f91dd55f8d7a1d2d5bf8df0fd65c1c9b866ffd
Jasjot Singh <jasjot784@gmail.com>
2020-07-23 18:26:57 +0530

    Added basic intent for chosing and getting a contact

--- app/src/main/java/com/jasjotsingh/contacts/MainActivity.java
+++ app/src/main/java/com/jasjotsingh/contacts/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = MainActivity.class.getSimpleName();
+    static final int REQUEST_SELECT_CONTACT = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 callUs();
+            }
+        });
+        findViewById(R.id.buttonPickContact).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectContact();
             }
         });
     }
@@ -29,5 +39,23 @@ public class MainActivity extends AppCompatActivity {
 
         if (intent.resolveActivity(getPackageManager())!=null)
             startActivity(intent);
+    }
+    public void selectContact() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_SELECT_CONTACT);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_SELECT_CONTACT && resultCode == RESULT_OK) {
+            Uri contactUri = data.getData();
+            // Do something with the selected contact at contactUri
+            Toast.makeText(this, contactUri.toString(), Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "onActivityResult: "+contactUri);
+
+        }
     }
 }
